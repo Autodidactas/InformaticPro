4763040077c1210db968fcd8516866c4ac9dd048
sacrac <crocha09.09@gmail.com>
2012-09-13 17:08:49 -0600

    probando la fecha del countdonw aun le falta toy seguro :P

--- sitio/views.py
+++ sitio/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from sitio.models import *
+import datetime
 # Create your views here.
 
 def index(request):
@@ -10,6 +11,15 @@ def index(request):
 	lista = []
 	for curso in Cursos.objects.all():
 		lista.append(Videos.objects.filter(curso=curso))
+
+	#calculo de los dias que faltan para el proximo taller
+	fecha_1 = datetime.date.today()
+	fecha_2 = [nose.fecha for nose in domingo][0]
+	fecha_final = fecha_2 - fecha_1
+	horas = datetime.datetime.today().time().__str__().split('.')[0] 	
+	nose = ('%s:%s') % (fecha_final.days,horas)
+	#------------------------------------------------------
+	fecha_actual = nose
 
 	return render_to_response('base.html', locals(),
 							   context_instance=RequestContext(request))
